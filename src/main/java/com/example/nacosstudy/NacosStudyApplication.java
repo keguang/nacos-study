072914e559b6fd4e09e1329780f749bc963c2076
keguang <keguang@eano.com>
2023-10-04 12:03:18 +0800

    first commit

---
+++ src/main/java/com/example/nacosstudy/NacosStudyApplication.java
@@ -0,0 +1,12 @@
+package com.example.nacosstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosStudyApplication.class, args);
+    }
+}
